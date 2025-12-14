5c55fad44d01093d3a9698cc066dd8c2d14afa43
George-Irinel-Necula <irinelnecula@yahoo.com>
2025-12-14 17:36:56 +0200

    Fixed theme switcher load state

--- src/ThemeSwitcher.jsx
+++ src/ThemeSwitcher.jsx
@@ -3,10 +3,10 @@ import {useTheme} from "@heroui/use-theme";
 import { IconMoon, IconMoonFilled, IconSunFilled } from "@tabler/icons-react";
 
 export const ThemeSwitcher = () => {
-  const { theme, setTheme } = useTheme()
+  const { theme, setTheme } = useTheme("dark")
   return (
     <div>
-      <Switch size="sm" classNames={{thumb:"black"}} thumbIcon={theme=="dark"?<IconMoonFilled></IconMoonFilled>:<IconSunFilled></IconSunFilled>} defaultSelected onChange={()=>{theme=="light"?setTheme('dark'):setTheme('light')}}></Switch>
+      <Switch size="sm" classNames={{thumb:"black"}} thumbIcon={theme=="dark"?<IconMoonFilled></IconMoonFilled>:<IconSunFilled></IconSunFilled>} isSelected={theme=="dark"?true:false} onChange={()=>{theme=="light"?setTheme('dark'):setTheme('light')}}></Switch>
     </div>
   )
 };
